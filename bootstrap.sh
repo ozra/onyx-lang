25fffa00e0b869e4f89a12065f49f7dbe328d49e
Oscar Campbell <oscar@campbell.nu>
2016-04-14 01:43:17 +0200

    Slight bootstrap changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,13 +29,13 @@ CRYSTAL_VERSION="0.15.0"
 echo "Gets Crystal Version '${CRYSTAL_VERSION}'"
 
 # curl can't handle the redirect to data-store!
-wget "https://github.com/manastech/crystal/releases/download/${CRYSTAL_VERSION}/crystal-${CRYSTAL_VERSION}-1-linux-x86_64.tar.gz" -O - | tar zx
+wget "https://github.com/manastech/crystal/releases/download/${CRYSTAL_VERSION}/crystal-${CRYSTAL_VERSION}-1-linux-x86_64.tar.gz" -O - | tar zx || exit 1
 echo "Installs Crystal ${CRYSTAL_VERSION} at /opt/cr-ox/"
 
 # sudo mkdir -p /opt
 sudo rm -rf /opt/cr-ox
-sudo cp -a "crystal-${CRYSTAL_VERSION}-1/" /opt/cr-ox
-sudo mv /opt/cr-ox/bin/crystal /opt/cr-ox/bin/cr-ox
+sudo cp -a "crystal-${CRYSTAL_VERSION}-1/" /opt/cr-ox || exit 1
+sudo mv /opt/cr-ox/bin/crystal /opt/cr-ox/bin/cr-ox || exit 1
 sudo ln -fs /opt/cr-ox/bin/cr-ox /usr/local/bin/cr-ox
 
 rm -rf "crystal-${CRYSTAL_VERSION}-1"
@@ -60,7 +60,7 @@ echo ""
 
 # CRYSTAL_CONFIG_PATH=`pwd`/src \
 #     ./bin/cr-ox build --release -o .build/onyx src/compiler/onyx.cr
-make all
+make all || exit 1
 
 echo "Installs Onyx at /opt/onyx/"
 echo ""
