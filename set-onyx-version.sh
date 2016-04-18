c2604015608c173369d84ed716fdb1a338fbeb5b
Oscar Campbell <oscar@campbell.nu>
2016-04-18 18:28:17 +0200

    Refine version "bump" script

--- set-onyx-version.sh
+++ set-onyx-version.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-echo "ONYX_VERSION = \"$1\"" > src/compiler/onyx/version_number.cr
-git tag "$1"
+if [ $1 ]; then
+    echo "ONYX_VERSION = \"$1\"" > src/compiler/onyx/version_number.cr
+    git tag "$1"
+    $0
+else
+    cat src/compiler/onyx/version_number.cr
+fi
